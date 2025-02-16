84515635858e6e9c2167a8216c636a31dd445765
remihuigen <remi@bureaubox.nl>
2025-02-16 21:57:01 +0100

    move dep to devdeps

--- scripts/directus_datahub.ts
+++ scripts/directus_datahub.ts
@@ -1,80 +1,81 @@
 import type { Product, ProductForm, Organization, Location } from '../types';
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const hoviOrganizations: Organization[] = [];
+const mboOrganizations: Organization[] = [];
 
-const directoryPath = path.join(__dirname, '../output/hovi');
-const files = fs.readdirSync(directoryPath);
+const directoryPathHoviOrgs = path.join(__dirname, '../output/hovi/organizations');
+const filesHOVI = fs.readdirSync(directoryPathHoviOrgs);
 
-files.forEach(file => {
+const directoryPathMboOrgs = path.join(__dirname, '../output/kiesmbo/organizations');
+const filesMBO = fs.readdirSync(directoryPathMboOrgs);
+
+filesHOVI.forEach(file => {
     if (path.extname(file) === '.json') {
-        const filePath = path.join(directoryPath, file);
+        const filePath = path.join(directoryPathHoviOrgs, file);
         const rawData = fs.readFileSync(filePath, 'utf-8');
         const organization: Organization = JSON.parse(rawData);
         hoviOrganizations.push(organization);
     }
 });
 
-function getMostUsedLocation(products: Array<Product & { location: Location | null }>): Location | null {
-    const locationCount: { [key: string]: { location: Location, count: number } } = {};
-
-    products.forEach(product => {
-        if (product.location) {
-            const locationKey = JSON.stringify(product.location);
-            if (locationCount[locationKey]) {
-                locationCount[locationKey].count++;
-            } else {
-                locationCount[locationKey] = { location: product.location, count: 1 };
-            }
-        }
-    });
-
-    let mostUsedLocation: Location | null = null;
-    let maxCount = 0;
-
-    for (const key in locationCount) {
-        if (locationCount[key].count > maxCount) {
-            maxCount = locationCount[key].count;
-            mostUsedLocation = locationCount[key].location;
-        }
+filesMBO.forEach(file => {
+    if (path.extname(file) === '.json') {
+        const filePath = path.join(directoryPathMboOrgs, file);
+        const rawData = fs.readFileSync(filePath, 'utf-8');
+        const organization: Organization = JSON.parse(rawData);
+        mboOrganizations.push(organization);
     }
+});
 
-    return mostUsedLocation;
-}
+const locationsMBO = JSON.parse(fs.readFileSync(path.join(__dirname, '../output/kiesmbo/locations/locations_with_geodata.json'), 'utf-8'))
+const locationsHOVI = JSON.parse(fs.readFileSync(path.join(__dirname, '../output/hovi/locations/locations_with_geodata.json'), 'utf-8'))
 
-
-const sanitizedLocations: Location[] = []
-
-const sanitizedOrganizations = hoviOrganizations.map(org => {
-
-    const main_location = getMostUsedLocation(org.products);
-
-    const locations = org.products.map(product => (product.location as Location));
-
-    console.log(locations.filter(loc => !loc.hovi_id).length);
-
-    const uniqueLocations = Array.from(new Map(locations.map(loc => [loc.hovi_id, loc])).values());
-
-    sanitizedLocations.push(...uniqueLocations);
-
-    const { products, ...rest } = org;
-
-    return {
-        ...rest,
-        locations: uniqueLocations.map(location => location.hovi_id),
-        main_location: main_location ? main_location.hovi_id : null
-    }
-})
-
-
-const uniqueSanitizedLocations = Array.from(new Map(sanitizedLocations.map(loc => [loc.hovi_id, loc])).values());
 
 console.log({
-    sanitizedOrganizations: sanitizedOrganizations.length,
-    uniqueSanitizedLocations: uniqueSanitizedLocations.length
+    hoviOrganizations: hoviOrganizations.length,
+    mboOrganizations: mboOrganizations.length,
+    totalOrganizations: hoviOrganizations.length + mboOrganizations.length,
+    locationsMBO: locationsMBO.length,
+    locationsHOVI: locationsHOVI.length,
+    totalLocations: locationsMBO.length + locationsHOVI.length
 })
 
-fs.writeFileSync('../output/datahub/hovi/organizations.json', JSON.stringify(sanitizedOrganizations, null, 2));
-fs.writeFileSync('../output/datahub/hovi/locations.json', JSON.stringify(uniqueSanitizedLocations, null, 2));
+
+
+// const sanitizedLocations: Location[] = []
+
+// const sanitizedOrganizations = hoviOrganizations.map(org => {
+
+//     const locations = org.products.map(product => (product.location as Location));
+
+//     console.log(locations.filter(loc => !loc.hovi_id).length);
+
+//     const uniqueLocations = Array.from(new Map(locations.map(loc => [loc.hovi_id, loc])).values());
+
+//     sanitizedLocations.push(...uniqueLocations);
+
+//     const { products, ...rest } = org;
+
+//     return {
+//         ...rest,
+//         locations: uniqueLocations.map(location => location.hovi_id),
+//     }
+// })
+
+
+// const uniqueSanitizedLocations = Array.from(new Map(sanitizedLocations.map(loc => [loc.hovi_id, loc])).values());
+
+// console.log({
+//     sanitizedOrganizations: sanitizedOrganizations.length,
+//     uniqueSanitizedLocations: uniqueSanitizedLocations.length
+// })
+
+// fs.writeFileSync('../output/datahub/hovi/organizations.json', JSON.stringify(sanitizedOrganizations, null, 2));
+// fs.writeFileSync('../output/datahub/hovi/locations.json', JSON.stringify(uniqueSanitizedLocations, null, 2));
 
